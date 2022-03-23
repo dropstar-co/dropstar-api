73e35ffe09b387984d6b49dec6a3d65279ad4734
Jorge Castro-Gallardo <jcastro.inf@gmail.com>
2022-03-23 11:05:44 +0100

    Hook the user wallet in the user creation
    Signed-off-by: Jorge Castro-Gallardo <jcastro.inf@gmail.com>

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,14 +1,19 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    VenlyUID: DataTypes.STRING,
-    Email: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
+  const User = sequelize.define(
+    'User',
+    {
+      VenlyUID: DataTypes.STRING,
+      Email: DataTypes.STRING,
+      walletAddress: DataTypes.STRING,
+    },
+    {},
+  );
+  User.associate = function (models) {
     // associations can be defined here
     User.hasMany(models.Bid, {
-      foreignKey:'userID'
-    })
+      foreignKey: 'userID',
+    });
   };
   return User;
 };
--- src/modules/auth/authController.js
+++ src/modules/auth/authController.js
@@ -1,11 +1,11 @@
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { v4 as uuidv4 } from "uuid";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { v4 as uuidv4 } from 'uuid';
 
-import SendGridHelper from "./../../helpers/sendGridHandler";
-import errorHandler from "./../../helpers/errorHandler";
-import models from "./../../database/models";
-import responseHandler from "../../helpers/responseHandler";
+import SendGridHelper from './../../helpers/sendGridHandler';
+import errorHandler from './../../helpers/errorHandler';
+import models from './../../database/models';
+import responseHandler from '../../helpers/responseHandler';
 
 class AuthController {
   static async createUser(req, res) {
@@ -13,30 +13,33 @@ class AuthController {
       const userExists = await models.User.findOne({
         where: { Email: req.body.Email },
       });
-      const token = jwt.sign({email:req.body.Email}, process.env.SECRET_KEY,{expiresIn:'7d'})
-      console.log(token)
+      const token = jwt.sign({ email: req.body.Email }, process.env.SECRET_KEY, {
+        expiresIn: '7d',
+      });
+      console.log(token);
       if (!userExists) {
         const user = await models.User.create({
           Email: req.body.Email,
           VenlyUID: req.body.VenlyUID,
+          walletAddress: req.body.walletAddress,
         });
 
         // SendGridHelper.sendConfirmationMail(token, req.body.email);
         const data = {
-          user, 
-          token
-        }
+          user,
+          token,
+        };
         return res.status(201).json({
           success: true,
-          message: "User registered successfully.",
+          message: 'User registered successfully.',
           user: user,
         });
       }
       const data = {
-        userExists, 
-        token
-      }
-      return responseHandler(res, "Logged success", 200, userExists);
+        userExists,
+        token,
+      };
+      return responseHandler(res, 'Logged success', 200, userExists);
     } catch (error) {
       errorHandler.handleError(error.message, 500, res);
     }
