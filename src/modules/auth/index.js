d0d1f2a9fe88a18a83a94b71628f192d655acccf
Jorge Castro-Gallardo <jcastro.inf@gmail.com>
2022-03-28 18:22:13 +0200

    Add endpoint to update user profile
    Signed-off-by: Jorge Castro-Gallardo <jcastro.inf@gmail.com>

--- src/modules/auth/index.js
+++ src/modules/auth/index.js
@@ -17,29 +17,27 @@ import AuthController from './authController';
  *     example: 'jayaramachandran@whizpath.com'
  */
 const Router = express.Router();
- /**
-  * @swagger
-  * /user/info:
-  *  post:
-  *   summary: Get user logged 
-  *   tags: 
-  *     - User functionality
-  *   description: Get user logged 
-  *   requestBody:
-  *    content:
-  *     application/json:
-  *      schema:
-  *       $ref: '#/definitions/User'
-  *   responses:
-  *    201:
-  *     description: user created succesfully
-  *    500:
-  *     description: failure in creating parent
-  */
-Router.post(
-    '/user/info',
-    AuthController.createUser
-);
+/**
+ * @swagger
+ * /user/info:
+ *  post:
+ *   summary: Get user logged
+ *   tags:
+ *     - User functionality
+ *   description: Get user logged
+ *   requestBody:
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/definitions/User'
+ *   responses:
+ *    201:
+ *     description: user created succesfully
+ *    500:
+ *     description: failure in creating parent
+ */
+Router.post('/user/info', AuthController.createUser);
 
+Router.post('/user/update', AuthController.updateUser);
 
 export default Router;
