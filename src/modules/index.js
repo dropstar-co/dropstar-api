47f2181b47d78320642db23b883e7b6eb8db4bb4
Jorge Castro-Gallardo <jcastro.inf@gmail.com>
2022-03-29 03:39:59 +0200

    Reading SaleVouchers from database
    Signed-off-by: Jorge Castro-Gallardo <jcastro.inf@gmail.com>

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,15 +1,17 @@
 import auth from './auth';
 
-import Bids from './Bids'
-import nft from './nft'
+import Bids from './Bids';
+import nft from './nft';
+import SaleVoucher from './SaleVoucher';
 
 const apiPrefix = '/api/v1';
 
-const routes = (app) => {
-    app.use(apiPrefix, auth);
-    app.use(apiPrefix, Bids);
-    app.use(apiPrefix, nft);
-    return app;
-}
+const routes = app => {
+  app.use(apiPrefix, auth);
+  app.use(apiPrefix, Bids);
+  app.use(apiPrefix, nft);
+  app.use(apiPrefix, SaleVoucher);
+  return app;
+};
 
 export default routes;
